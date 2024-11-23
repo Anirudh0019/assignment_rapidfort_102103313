db17cf4e577182e9bca9324b2a333afe36d09d10
Anirudh0019 <anirudhk0019@gmail.com>
2024-11-24 00:25:42 +0530

    removed label

--- frontend/src/components/FileInfo.jsx
+++ frontend/src/components/FileInfo.jsx
@@ -27,12 +27,12 @@ export default function FileInfo({ file, darkMode }) {
             onChange={(e) => setShowRaw(e.target.checked)}
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           />
-          <label
+          {/* <label
           htmlFor="raw-checkbox"
           className={`ms-2 text-sm font-medium ${darkMode ? 'text-slate-300' : 'text-slate-900'}`}
         >
           raw
-        </label>
+        </label> */}
         </div>
       </div>
 
