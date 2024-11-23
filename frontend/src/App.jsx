01feac372e497e3f994e429b177ff1a57c02bfc6
Anirudh0019 <anirudhk0019@gmail.com>
2024-11-24 00:12:50 +0530

    Prod Showdata

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       const formData = new FormData();
       formData.append('file', selectedFile);
 
-      const response = await fetch('http://localhost:3000/convert', {
+      const response = await fetch('/convert', {
         method: 'POST',
         body: formData,
       });
--- frontend/src/components/FileInfo.jsx
+++ frontend/src/components/FileInfo.jsx
@@ -28,11 +28,11 @@ export default function FileInfo({ file, darkMode }) {
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           />
           <label
-            htmlFor="raw-checkbox"
-            className={`${darkMode ? 'text-white' : 'text-gray-700' } ms-2 text-sm font-medium`}
-          >
-            raw
-          </label>
+          htmlFor="raw-checkbox"
+          className={`ms-2 text-sm font-medium ${darkMode ? 'text-slate-300' : 'text-slate-900'}`}
+        >
+          raw
+        </label>
         </div>
       </div>
 
